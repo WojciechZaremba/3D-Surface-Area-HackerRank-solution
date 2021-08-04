37e63d494ee0dda4efc4ce4e29758651b0ba37ce
WojciechZaremba <66971285+WojciechZaremba@users.noreply.github.com>
2021-08-04 22:17:46 +0200

    formatting
    "ogarnij kod" na jsfiddle

--- working-solution.js
+++ working-solution.js
@@ -1,49 +1,49 @@
 function surfaceArea(A) {
-  const flatArray = A.reduce((a,c) => a.concat(c),[])
-  
+  const flatArray = A.reduce((a, c) => a.concat(c), [])
+
   const buildHeight = Math.max(...flatArray)
   const columnsNum = flatArray.length
-  
+
   let area = 0
-  
+
   // MAIN CODE:
   for (let floor = buildHeight; floor > 0; floor--) {
-  	const currentFloorArea = findCurrentFloorArea(floor)
-  	area += currentFloorArea
+    const currentFloorArea = findCurrentFloorArea(floor)
+    area += currentFloorArea
   }
-  
+
   area += columnsNum * 2
-  
+
   // METHODS:
-	function findCurrentFloorArea(floor) {
-  	const currentFloorMap = createCurrentFloorMap(floor)
+  function findCurrentFloorArea(floor) {
+    const currentFloorMap = createCurrentFloorMap(floor)
     const currentFloorSurfaceMap = createSurfaceMap(currentFloorMap)
     const sumOfFloorWalls = currentFloorSurfaceMap.reduce((acc, curr) => {
-      return acc + curr.reduce((a,c) => a += c, 0)
+      return acc + curr.reduce((a, c) => a += c, 0)
     }, 0)
-    
+
     //console.log("4 = room, 0 = empty space")
-  	//console.log(currentFloorMap)
+    //console.log(currentFloorMap)
     //console.log("4,3,2,1,0 = number of outside walls")
     //console.log(currentFloorSurfaceMap)
     //console.log(sumOfFloorWalls)
-    
+
     return sumOfFloorWalls
   }
-  
+
   function createCurrentFloorMap(floor) {
-  	let map = []
+    let map = []
     map = A.map(a => {
-    	return a.map((n) => n >= floor ? 4 : 0)
-    })    
-  	return map
+      return a.map((n) => n >= floor ? 4 : 0)
+    })
+    return map
   }
-  
+
   function createSurfaceMap(floorMap) {
     const outsideWallsMap = floorMap.map(a => {
       return a.map(r => r)
     })
-    
+
     for (let i = 0; i < floorMap.length; i++) {
       for (let j = 0; j < floorMap[0].length; j++) {
         if (floorMap[i][j] && floorMap[i][j + 1] > 0) outsideWallsMap[i][j]--
@@ -51,15 +51,17 @@ function surfaceArea(A) {
         if (floorMap[i][j] && floorMap[i + 1] && floorMap[i + 1][j] > 0) outsideWallsMap[i][j]--
         if (floorMap[i][j] && floorMap[i - 1] && floorMap[i - 1][j] > 0) outsideWallsMap[i][j]--
       }
-      
+
     }
     return outsideWallsMap
-    
+
   }
-	return area
+  return area
 }
 
-const array =[[10]]
+const array = [
+  [10]
+]
 //const array = [[15],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1]]
 //const array = [[3,7,1],
 //              [3,2,6],
